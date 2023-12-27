b84c0e5b3e2d7f4cd7de4da287d05988dc055595
RafalNicki <Nicol79@op.pl>
2023-12-27 12:54:58 +0100

    first commit

---
+++ src/main/java/com/kodilla/currency_exchange_beckend/CurrencyExchangeBeckendApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.currency_exchange_beckend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeBeckendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyExchangeBeckendApplication.class, args);
+    }
+
+}
